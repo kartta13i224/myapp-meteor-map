a8795391d7754e268a2f31d192ce130ada797d80
kartta13i224 <tommi6630@luukku.com>
2016-09-21 12:51:11 +0300

    HTML_template+method_start
    Added html template base for city selection.
    Start of meteorite method creations.

--- client/Client.js
+++ client/Client.js
@@ -1,13 +1,66 @@
 
 import './Client.html';
 
-Template.hello.onCreated(function helloOnCreated(){
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
+
+
+Template.leadForm.helpers({
+    'categories': function(){
+        return Options.find();
+    },
+    'isSelected': function(option){
+      var selected = Selected.findOne('SELECTED') ? Selected.findOne('SELECTED').selected : '';
+      return option === selected ? 'selected' : '';
+    },
+	'categories2': function(){
+        return Options2.find();
+    },
+    'isSelected2': function(option2){
+      var selected2 = Selected2.findOne('SELECTED2') ? Selected2.findOne('SELECTED2').selected2 : '';
+      return option2 === selected2 ? 'selected2' : '';
+    }
+  });
+
+  Template.leadForm.events({
+    'click #addOption': function () {
+      Options.insert({option: Fake.sentence(3)});
+    },
+    'change #leadSource2': function(event, template){
+      Selected.update('SELECTED', {selected: event.target.value});
+    },
+	    'click #addOption2': function () {
+      Options2.insert({option: Fake.sentence(5)});
+    },
+    'change #leadSource2': function(event, template){
+      Selected2.update('SELECTED2', {selected2: event.target.value});
+    }
+  })
+
+
+
+Template.info.onCreated(function helloOnCreated(){
 	
 	Session.set('cityText', 'ALOITUS TEKSTI');
 });
 
 
-Template.hello.helpers({
+Template.info.helpers({
 		
 	city(){
 		return Session.get('cityText');
@@ -15,11 +68,10 @@ Template.hello.helpers({
 });
 	
 	
-Template.hello.events({
+Template.info.events({
 	'click .test'(event, instance){
 			
-		// Start of call
-		Meteor.call('simpleFunction', {
+		Meteor.call('calculateDistance', {
 			city1: 'Jämsä',
 			city2: 'Tampere'
 			}, (err, res) =>{
@@ -29,7 +81,21 @@ Template.hello.events({
 					// No errors in input!
 					console.log(res);
 				}
+			
 		});
+			
+			
+		// Start of call
+		
+		Meteor.call('getCityList', {}, (err, res) =>{
+				if (err){
+					alert(err);
+				} else {
+					// No errors in input!
+					console.log(res);
+				}
+		});
+		
 		// End of call
 	},
 });
--- server/Server.js
+++ server/Server.js
@@ -4,10 +4,16 @@
 
 Meteor.startup(() => {
   // Create map database
+		
+		/*
+		if (suomi_gps)
+			suomi_gps._dropCollection();
+		*/
+		
 		suomi_gps = new Mongo.Collection('suomi_gps');
 		console.log("METEORITE ON AUKI");
 		// Code ran only once, commented for future purposes if recreating it.
-		//suomi_gps._dropCollection();
+		
 		/*
 		suomi_gps.insert({place1:"Oulu",place2:"Kajaani",dist:180});
 		suomi_gps.insert({place1:"Oulu",place2:"Kuopio",dist:290});
@@ -51,18 +57,95 @@ Meteor.startup(() => {
 		suomi_gps.insert({place1:"Helsinki",place2:"Imatra",dist:260});
 		suomi_gps.insert({place1:"Helsinki",place2:"Turku",dist:160});
 		*/
+		
 });
 
 // Laskenta koodi.
+
+
 Meteor.methods({
-	'getCityList'({city1, city2}){
+	
+	// Koodi palauttaa tietokannassa olevan listan kaikista eri kaupungeista.
+	'getCityList'({}){
+		
+		// Etsii MongoDB:stä listan kaupungeista joilla on eri nimi ja niiltä löytyy 
 		var distinctCities = _.uniq(suomi_gps.find({},{
 			sort: {place1: 1}, fields: {place1: true}
 			}).fetch().map(function(x){
 				return x.place1;
 		}), true);
 	
-		console.log(city1 + " ja " + city2);
 		return distinctCities;
+	},
+	
+	// Ottaa sisäänsä kahden kaupungin tiedot ja laskee niiden välisen lyhimmän välimatkan.
+	'calculateDistance'({city1, city2}){
+		
+		// Haetaan kaikki data mongosta ja sijoitetaan listaan.
+		var cities = suomi_gps.find().fetch();
+		
+		// Karsitaan aluepaikkaan sitoutuneet edestakaisuudet.
+		// Place1 = aloitus paikka, Place2 = seuraava paikka.
+		// if ("Place1 = Seuraava paikka, Place1 = aloituspaikka" == totta) DELET THIS
+		for (var cityLoop = 0; cityLoop < cities.length; cityLoop++){
+			if (cities[cityLoop].place2 == "city1"){
+				cities.splice(cityLoop, 1);
+				cityLoop--;
+			}
+		}
+		
+		// Tulostus client päälle testiksi.
+		return cities;
+		
+		
+		
+		
+		// Aloitetaan katselmointi ensimmäisestä pisteestä aloittaen. Loop.
+		// Kun jonkin pisteen seuraavat ehdot kaikki täyttyvät, piste karsitaan pois (DELET THIS):
+		// - Kyseisen pisteen jokainen reitti on katselmoitu lävitse.
+		// - Kyseisen pisteen jokaisen päässä olevan pisteen reitit on katselmoitu lävitse.
+		// - Kyseisten edelliste mainitsemien pisteiden reitit ovat todettu pitemmiksi kuin muut vaihtoehdot.
+		
+		// Kun lyhin reitti on löydetty, saadaan se Path muuttujan kautta tulostettua,
+		// aloittaen päätöspisteestä.
+		
+	
+		
+		/*
+			Place1 = City1
+			Place2 = City2
+			
+			place1
+			suomi_gps.insert({place1:"Oulu",place2:"Kajaani",dist:180});
+			suomi_gps.insert({place1:"Oulu",place2:"Kuopio",dist:290});
+			suomi_gps.insert({place1:"Oulu",place2:"Vaasa",dist:320});
+			
+			
+			
+			!place2
+			suomi_gps.insert({place1:"Jämsä",place2:"Kuopio",dist:203});
+			suomi_gps.insert({place1:"Jämsä",place2:"Mikkeli",dist:167});
+			suomi_gps.insert({place1:"Jämsä",place2:"Tampere",dist:100});
+			suomi_gps.insert({place1:"Jämsä",place2:"Helsinki",dist:225});
+			
+		*/
+		
+		// place = place of the object.
+		// known = if the is known and checked. F = false, T = true.
+		// Dt = Total Distance from starting point.
+		// Path = From which place the Total Distance is calculated from.
+		/*
+		var distList = [
+			{place:"", known:"F", Dt:"", Path:""},
+			{place:"", known:"F", Dt:"", Path:""},
+			{place:"", known:"F", Dt:"", Path:""},	
+			{place:"", known:"F", Dt:"", Path:""},
+		
+		];
+		*/
+		
 	}
+	
+	
+	
 });
